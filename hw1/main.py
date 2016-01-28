0209a8898546cd103903c4aa1ffec739aec6cb07
merajahmed <merajkhan@outlook.com>
2016-01-28 18:53:14 -0500

    dumping centrality measures to single file for comparison

--- hw1/main.py
+++ hw1/main.py
@@ -1,14 +1,15 @@
 import networkx as nx
 import csv
-__author__ = 'meraj'
+from collections import OrderedDict
 
-class data_network:
+__author__ = 'anirban & meraj'
 
+
+class DataNetwork:
     GRAPH_TYPE_DICT = {'directed': nx.DiGraph,
-                  'undirected': nx.Graph,
-                  'multidirected': nx.MultiDiGraph,
-                  'multiundirected': nx.MultiGraph}
-
+                       'undirected': nx.Graph,
+                       'multidirected': nx.MultiDiGraph,
+                       'multiundirected': nx.MultiGraph}
     CENTRALITY_DICT = {'in_degree': nx.in_degree_centrality,
                        'out_degree': nx.out_degree_centrality,
                        'degree': nx.degree_centrality,
@@ -27,13 +28,24 @@ class data_network:
             self.CENTRALITY_DICT.pop('in_degree', None)
             # None has been done to take care of exception in case key doesn't exist
             self.CENTRALITY_DICT.pop('out_degree', None)  # like wise
-
-        for centrality, function_name in self.CENTRALITY_DICT.items():
-            with open('{}_centrality_{}.csv'.format(centrality, self.data_type), 'wb') as csv_file:
-                writer = csv.writer(csv_file)
+        with open('{}_centrality.csv'.format(self.data_type), 'wb') as csv_file:
+            writer = csv.writer(csv_file)
+            centrality_measures = []
+            node_order = None
+            for centrality, function_name in self.CENTRALITY_DICT.items():
                 centrality_measure = function_name(self.G)
-                for node in centrality_measure:
-                    writer.writerow([node, centrality_measure[node]])
+                ordered_centrality_measure = OrderedDict(sorted(centrality_measure.items(), key=lambda x: x[0]))
+                centrality_measures.append(list(ordered_centrality_measure.values()))
+                if node_order is None:
+                    node_order = list(ordered_centrality_measure.keys())
+            csvheader = ['NodeID']
+            csvheader += list(self.CENTRALITY_DICT.keys())
+            writer.writerow(csvheader)
+            for i in range(len(node_order)):
+                centrality_values = [node_order[i]]
+                for j in range(len(centrality_measures)):
+                    centrality_values.append(centrality_measures[j][i])
+                writer.writerow(centrality_values)
 
     def clustering_coefficient(self):
         undirected_G = self.G.to_undirected()
@@ -43,8 +55,9 @@ class data_network:
             for node in clustering_dict:
                 writer.writerow([node, clustering_dict[node]])
 
+
 def main():
-    wiki_vote = data_network('dataset/Wiki-Vote.txt', 'wiki', 'directed')
+    wiki_vote = DataNetwork('dataset/Wiki-Vote.txt', 'wiki', 'directed')
     wiki_vote.centrality()
     wiki_vote.clustering_coefficient()
 
