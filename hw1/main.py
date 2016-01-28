1a8881711853437da64da54341f710641f88d1f6
Anirban Gupta <anirban1992@gmail.com>
2016-01-28 17:51:42 -0500

    Added clustering coefficient

--- hw1/main.py
+++ hw1/main.py
@@ -35,9 +35,18 @@ class data_network:
                 for node in centrality_measure:
                     writer.writerow([node, centrality_measure[node]])
 
+    def clustering_coefficient(self):
+        undirected_G = self.G.to_undirected()
+        with open('clustering_coefficients_{}.csv'.format(self.data_type), 'wb') as csv_file:
+            writer = csv.writer(csv_file)
+            clustering_dict = nx.clustering(undirected_G)
+            for node in clustering_dict:
+                writer.writerow([node, clustering_dict[node]])
+
 def main():
     wiki_vote = data_network('dataset/Wiki-Vote.txt', 'wiki', 'directed')
     wiki_vote.centrality()
+    wiki_vote.clustering_coefficient()
 
 if __name__ == '__main__':
     main()
