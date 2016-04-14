cae3f2319a70ba7b9f88e205e8b01a958b2d619c
merajahmed <merajkhan@outlook.com>
2016-04-14 19:24:44 -0400

    entropy, and degree centrality calculation

--- nbaparsing/interaction_graph.py
+++ nbaparsing/interaction_graph.py
@@ -1,6 +1,9 @@
 import csv
 
 import networkx as nx
+from collections import OrderedDict
+import operator
+import numpy as np
 
 __author__ = 'meraj'
 
@@ -32,6 +35,53 @@ def creategraph(graphfilename):
             G.add_edge(from_node, to_node, weight=currentweight+1)
     return G
 
+def calculate_degree_centrality(G):
+    node_degrees = dict()
+    for edge in G.edges():
+        node_1 = edge[0]
+        node_2 = edge[1]
+        weight = G.get_edge_data(node_1, node_2)
+        node_degrees[node_1] = node_degrees.get(node_1, 0)+weight['weight']
+        node_degrees[node_2] = node_degrees.get(node_2, 0)+weight['weight']
+    ordered_degrees = OrderedDict(sorted(node_degrees.items(),key=operator.itemgetter(1),reverse=True))
+    sum_degrees = sum(ordered_degrees.values())
+    for key in ordered_degrees:
+        ordered_degrees[key] = ordered_degrees[key]/float(sum_degrees)
+    ordered_degrees.pop('MADE')
+    ordered_degrees.pop('MISSED')
+    ordered_degrees.pop('Start')
+    graph_centrality = 0
+    max_degree = ordered_degrees.itervalues().next()
+    for node in ordered_degrees:
+        graph_centrality += (max_degree-ordered_degrees[node])
+    graph_centrality /= len(G.nodes())
+    return ordered_degrees, graph_centrality
+
+
+def calculate_entropy(G):
+    nodes = G.nodes()
+    nodes.remove('Start')
+    size = len(nodes)
+    total_weight = 0
+    probability_array = np.ndarray(shape=(size,size), dtype=float)
+    for i in range(size):
+        for j in range(size):
+            if G.has_edge(nodes[i], nodes[j]):
+                probability_array[i][j] = G.get_edge_data(nodes[i], nodes[j])['weight']
+            else:
+                probability_array[i][j] = 0
+            total_weight += probability_array[i][j]
+    probability_array /= total_weight
+    entropy = 0
+    for i in range(size):
+        for j in range(size):
+            p = probability_array[i][j]
+            if p != 0:
+                entropy -= p*np.log2(p)
+    return entropy
+
+
+
 G = creategraph('OSUvsIowaGraph.txt')
-for edge in G.edges():
-    print(edge, G.get_edge_data(edge[0], edge[1]))
+print(calculate_entropy(G))
+print(calculate_degree_centrality(G))
