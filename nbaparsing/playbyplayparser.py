17e7875ccab5093535edfa756c777f222f6685af
Anirban Gupta <anirban1992@gmail.com>
2016-04-14 21:35:38 -0400

    Changes to play_by_play logs

--- nbaparsing/playbyplayparser.py
+++ nbaparsing/playbyplayparser.py
@@ -1,6 +1,7 @@
 __author__ = 'anirban'
 import json
 import csv
+from collections import OrderedDict
 
 pass_data = json.load(open('playbyplay.json'))
 
@@ -8,6 +9,17 @@ headers = pass_data['resultSets'][0]['headers']
 header_names = headers[0:5] + headers[6:8] + headers[9:11] + headers[12:-3]
 
 event_dict = {}
+team_ids = []
+
+# Automated code to get home and visitor team id
+
+# gamedata = json.load(open('0021500492.json'))
+# print gamedata.keys()
+
+# team_id[0] = gamedata['events'][0]['home']['teamid']
+# team_id[1] = gamedata['events'][0]['visitor']['teamid']
+
+team_ids = ['1610612761', '1610612766']
 
 # 1 - shot (i and i+1 comparison)
 # 2 - miss and possible blocks ( again check i and i+1 events)
@@ -19,7 +31,6 @@ event_dict = {}
 # 6 - fouls - other event gets possession
 # 7 - violation - other event gets possession
 # 8 - substitution
-
 
 # s.foul -> opponent team had possession, retains possession
 
@@ -75,7 +86,7 @@ def rule_runner(event_id, sub_event_id, home_event, away_event):
         return team_flag, -1
 
     if event_id == 4:  # rebound
-        return -1, team_flag  # we know who gets the ball after rebound, but not who had the ball (determine later)
+        return team_flag, team_flag  # we know who gets the ball after rebound, but not who had the ball (determine later)
 
     if event_id in [6, 7]:  # for fouls, violations
         return -1, int(not team_flag)
@@ -85,37 +96,88 @@ def rule_runner(event_id, sub_event_id, home_event, away_event):
             return team_flag, int(not team_flag)
         else: # 11 stands for free throw 1 of 2
             return team_flag, team_flag
-    elif event_id == 5:  # turnovers
+
+    if event_id == 5:  # turnovers
         return team_flag, int(not team_flag)
-    elif event_id == 10:  # jump_ball
+
+    if event_id == 10:  # jump_ball
         return team_flag, team_flag
 
-# for i, row in enumerate(pass_data['resultSets'][0]['rowSet']):
-#     if row[2] not in event_dict:
-#         event_dict[row[2]] = {}
-#
-#     if row[3] not in event_dict[row[2]]:
-#         event_dict[row[2]][row[3]] = [row[6], row[7], row[9]]
 
-# print event_dict
-# print json.dumps(event_dict, sort_keys=True, indent=4)
+def possession_time(row):
+    # 1- gametime, 2 - event id,
+    # 0 - home_team, 1 - away_team
+    # -1 - we don't know
+    # -2 - shot, -3 - miss, -4 - steal
+    quarter, gametime, event_id, pre_flag, post_flag, home_event, away_event, player_1, player_1_team, player_2, player_2_team, player_3, player_3_team = row
 
-# for event_id in event_dict.keys():
-#     for sub_event_id in event_dict[event_id].keys():
-#         home_event = event_dict[event_id][sub_event_id][1]
-#         away_event = event_dict[event_id][sub_event_id][2]
-#         event_dict[event_id][sub_event_id].append(rule_runner(event_id, sub_event_id, home_event, away_event))
+    player_dict = OrderedDict((player, team) for player, team in zip([player_1, player_2, player_3],[player_1_team, player_2_team, player_3_team]))
+    if '0' in player_dict:
+        del player_dict['0']
+
+    # print player_dict
+
+    if int(event_id) == 10:  # jumpball
+        # print 'yolo'
+        players = [player for player in player_dict.keys() if player_dict[player] == team_ids[int(post_flag)]]
+        return [list(element) for element in zip([quarter] * 2, [gametime] * 2, players)]
+
+    if int(event_id) == 1:  # shots
+        return [[quarter, gametime, player_1], [quarter, gametime, -2], [quarter, gametime, post_flag]]
+
+    if int(event_id) == 2:  # misses
+        return [[quarter, gametime, player_1], [quarter, gametime, -3]]
+
+    if int(event_id) == 4:  # rebounds
+        return [[quarter, gametime, player_1]]
+
+    if int(event_id) in [6, 7]: # fouls, violations
+        return [[quarter, gametime, -1], [quarter, gametime, post_flag]]
+
+    if int(event_id) == 3:  # free-throws
+        my_list = []
+        if 'MISS' in home_event or 'MISS' in away_event:  # miss
+            my_list = [[quarter, gametime, player_1], [quarter, gametime, -3]]
+        else:
+            my_list = [[quarter, gametime, player_1], [quarter, gametime, -4]]
+
+        if pre_flag != post_flag:
+            my_list.append([quarter, gametime, post_flag])
+
+        return my_list
+
+    if int(event_id) == 5:  # turnovers
+        my_list = []
+        # print post_flag
+        pre_player = [player for player in player_dict.keys() if player_dict[player] == team_ids[int(pre_flag)]]
+        post_player = [player for player in player_dict.keys() if player_dict[player] == team_ids[int(post_flag)]]
+        # print post_player
+        if not pre_player:
+            my_list.append([quarter, gametime, pre_flag])
+        else:
+            my_list.append([quarter, gametime, pre_player[0]])
+
+        if not post_player:
+            my_list.append([quarter, gametime, pre_flag])
+        else:
+            my_list.append([quarter, gametime, pre_player])
+
+        return my_list
+
 
 with open('play_by_play.csv', 'wb') as play_file:
     play_writer = csv.writer(play_file)
-    play_writer.writerow(['GameClock', 'Who has the ball', 'Who will now have the ball', 'Home_event', 'Away_event', 'Player1', 'Player2', 'Player3'])
+    play_writer.writerow(['Quarter', 'GameClock', 'EventId', 'Who has the ball', 'Who will now have the ball', 'Home_event', 'Away_event', 'Player1', 'Player1_team_id', 'Player2', 'Player2_team_id', 'Player3', 'Player3_team_id'])
     for i, row in enumerate(pass_data['resultSets'][0]['rowSet']):
         # player_1 id 13, 15 team_id
         # player_2 id 20, 22 team_id
         # player_3 is 27, 29 team_id
 
-        if row[2] == 4:  # rebounds
-            continue
+        # if row[2] == 4:  # rebounds
+        #     continue
+
+        minutes, seconds = row[6].split(':')
+        row[6] = float(minutes) * 60 + float(minutes) % 60 + float(seconds)
 
         pre_possession_flag, post_possession_flag = rule_runner(row[2], row[3], row[7], row[9])
         if pre_possession_flag == -2 and post_possession_flag == -2:
@@ -127,39 +189,27 @@ with open('play_by_play.csv', 'wb') as play_file:
                 pre_flag, post_flag = rule_runner(next_row[2], next_row[3], next_row[7], next_row[9])
                 post_possession_flag = post_flag
 
-                play_writer.writerow([row[6], pre_possession_flag, post_possession_flag, row[7], row[9]])
-                continue
-                # skip writing the rebound event as it's useless, change the post possession flag for rebound
+                # play_writer.writerow([row[6], row[2], pre_possession_flag, post_possession_flag, row[7], row[9], row[13], row[20], row[27]])
+                # continue
+
                 # play_writer.writerow([next_row[6], pre_flag, post_flag, next_row[7], next_row[9]])
 
-        play_writer.writerow([row[6], pre_possession_flag, post_possession_flag, row[7], row[9]])
+        play_writer.writerow([row[4], row[6], row[2], pre_possession_flag, post_possession_flag, row[7], row[9], row[13], row[15], row[20], row[22], row[27], row[29]])
+#
+#
+with open('play_by_play.csv', 'rb') as play_file, open('possession.csv', 'wb') as poss_file:
+    writer = csv.writer(poss_file)
+    writer.writerow(['Quarter', 'Gametime', 'Player_Id'])
+    for index, row in enumerate(csv.reader(play_file)):
+        if index == 0:
+            continue
+        my_list = possession_time(row)
+        for row in my_list:
+            writer.writerow(row)
+        # break
 
 # print json.dumps(event_dict, sort_keys=True, indent=4)
 
 
 # player name or player id
 
-# with open('play_by_play_home.csv', 'wb') as home_file, open('play_by_play_away.csv', 'wb') as away_file, open('play_by_play_possession.csv', 'wb') as possession_file:
-#     home_writer = csv.writer(home_file)
-#     home_writer.writerow([name for name in header_names if name != 'VISITORDESCRIPTION'])
-#     away_writer = csv.writer(away_file)
-#     away_writer.writerow([name for name in header_names if name != 'HOMEDESCRIPTION'])
-#     possesion_writer = csv.writer(possession_file)
-#     possesion_writer.writerow(header_names)
-#     for row in pass_data['resultSets'][0]['rowSet']:
-#         # consider row[7], row[8], row[9] -> descriptions for home, neutral, away
-#         my_list = row[0:5]
-#         my_list.extend(row[6:8])
-#         my_list.extend(row[9:11])
-#         my_list.extend(row[12:-3])
-#
-#         if row[7] is not None and row[9] is not None:
-#             possesion_writer.writerow(my_list)
-#         elif row[7] is not None:
-#             del my_list[9]
-#             home_writer.writerow(my_list)
-#         elif row[9] is not None:
-#             del my_list[7]
-#             away_writer.writerow(my_list)
-
-32
