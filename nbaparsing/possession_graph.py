1eb973ad236295c785dc5e775d7db9932ec30d60
Anirban Gupta <anirban1992@gmail.com>
2016-04-21 13:30:07 -0400

    Minor changes to possession by player

--- nbaparsing/possession_graph.py
+++ nbaparsing/possession_graph.py
@@ -4,6 +4,8 @@ import json
 
 import networkx as nx
 from networkx.readwrite import json_graph
+
+
 def creategraph(possessionfilename):
     G = nx.DiGraph()
     start_nodes = [0, 1, -4, -5]
@@ -20,8 +22,8 @@ def creategraph(possessionfilename):
                 break
 
         for index, row in enumerate(file_reader):
-            first_site = prev_row[2]
-            second_site = row[2]
+            first_site = int(prev_row[2])
+            second_site = int(row[2])
 
             if second_site in start_nodes:
                 prev_row = row
@@ -38,7 +40,27 @@ def creategraph(possessionfilename):
 
 def jsonify_graph(graphobj, graphfile):
     G = graphobj
-    json.dump(json_graph.node_link_data(G), open(graphfile,'w'),indent=2)
+    start_nodes = [0, 1, -4, -5]
+    end_nodes = [-2, -3, -1, -6]
+    j = json_graph.node_link_data(G)
+    print j['nodes']
+    i = 0
+    k = 0
+
+    for id_pair in j['nodes']:
+        if id_pair['id'] in start_nodes:
+            i += 1
+            id_pair['x'] = 50
+            id_pair['y'] = 100 * i
+            id_pair['fixed'] = True
+
+        if id_pair['id'] in end_nodes:
+            k += 1
+            id_pair['x'] = 700
+            id_pair['y'] = 100 * k
+            id_pair['fixed'] = True
+
+    json.dump(j, open(graphfile, 'w'), indent=2)
 
 
 jsonify_graph(creategraph('possession.csv'), 'possession_graph.json')
