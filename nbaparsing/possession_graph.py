85972be27aa26087314c00b30f2c86a8a8a06731
Meraj Ahmed Khan <merajkhan@outlook.com>
2016-04-23 20:26:35 -0400

    vis json creation

--- nbaparsing/possession_graph.py
+++ nbaparsing/possession_graph.py
@@ -67,6 +67,41 @@ def jsonify_graph(graphobj, graphfile):
 
     json.dump(j, open(graphfile, 'w'), indent=2)
 
+def jsonify_vis(graphobj, graphfile):
+    G = graphobj
+    visgraph = dict()
+    start_nodes = [0, 1, -4, -5]
+    end_nodes = [-2, -3, -1, -6]
+    start_count = 0
+    end_count = 0
+    visgraph['nodes'] = list()
+    visgraph['edges'] = list()
+    start_y = [300, 150, -150, -300]
+    end_y = [300, 150, -150, -300]
+    num_nodes = len(G.nodes()) - 8
+    angle_multiplier = 0
+    slices = 2*math.pi/num_nodes
+    radius = 300
+    for node in G.nodes():
+        if node in start_nodes:
+            visgraph['nodes'].append({'id': node, 'x': -400, 'y': start_y.pop(), 'group': 'start', 'fixed': True})
+            print('start')
+        elif node in end_nodes:
+            visgraph['nodes'].append({'id': node, 'x': 400, 'y': end_y.pop(), 'group': 'end', 'fixed': True})
+        else:
+            angle = slices * angle_multiplier
+            visgraph['nodes'].append({'id': node, 'x': int(radius*math.cos(angle)), 'y': int(radius*math.sin(angle)), 'group': 'players'})
+            # visgraph['nodes'].append({'id': node})
+            angle_multiplier += 1
+    max_weight = max([G.get_edge_data(edge[0], edge[1])['weight'] for edge in G.edges()])
+    min_weight = min([G.get_edge_data(edge[0], edge[1])['weight'] for edge in G.edges()])
+    for edge in G.edges():
+        if edge[0] == edge[1]:
+            continue
+        weight = G.get_edge_data(edge[0], edge[1])['weight']
+        color = {'color':'red', 'opacity':(weight-min_weight)/float(max_weight-min_weight)}
+        visgraph['edges'].append({'from': edge[0], 'to': edge[1], 'value': weight, 'arrows': 'to','color':color,'smooth':False})
+    json.dump(visgraph, open(graphfile, 'w'), indent=2)
 
 jsonify_graph(creategraph('possession.csv'), 'possession_graph.json')
 # print G.edge
